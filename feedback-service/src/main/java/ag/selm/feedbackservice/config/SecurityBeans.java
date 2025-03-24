2bfacd1e620d216c1d30fddd6097fab40b5e7f29
Jokail <jokailxx@gmail.com>
2025-03-24 17:58:11 +0200

    add keycloak for customers

---
+++ feedback-service/src/main/java/ag/selm/feedbackservice/config/SecurityBeans.java
@@ -0,0 +1,25 @@
+package ag.selm.feedbackservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+
+@Configuration
+public class SecurityBeans {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+        return httpSecurity
+                .authorizeExchange(configurer -> configurer.anyExchange().authenticated())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+                .oauth2ResourceServer(customizer ->
+                        customizer.jwt((Customizer.withDefaults())))
+                .build();
+    }
+
+}
